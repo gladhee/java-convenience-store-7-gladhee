78561eba5fe33057cd111c4d8ba06391e362840a
gladhee <gladhee@icloud.com>
2024-11-10 17:59:38 +0900

    refactor(Product): Builder 패턴으로 수정 및 수량추가

--- src/main/java/store/domain/product/Product.java
+++ src/main/java/store/domain/product/Product.java
@@ -1,28 +1,34 @@
 package store.domain.product;
 
+import store.domain.stock.Stock.Builder;
+
 public class Product {
 
     private final String name;
     private final int price;
+    private int quantity;
 
-    private Product(String name, int price) {
-        validateProduct(name, price);
-        this.name = name;
-        this.price = price;
+    private Product(Builder builder) {
+        this.name = builder.name;
+        this.price = builder.price;
+        this.quantity = builder.quantity;
+        validateProduct();
     }
 
-    public static Product of(String name, int price) {
-        return new Product(name, price);
-    }
-
-    // @todo refactor
-    private void validateProduct(String name, int price) {
+    private void validateProduct() {
         if (name == null || name.trim().isEmpty()) {
             throw new IllegalArgumentException("[ERROR] 상품명은 빈 값일 수 없습니다.");
         }
         if (price <= 0) {
             throw new IllegalArgumentException("[ERROR] 상품 가격은 0보다 커야 합니다.");
         }
+        if (quantity < 0) {
+            throw new IllegalArgumentException("[ERROR] 상품 수량은 음수일 수 없습니다.");
+        }
+    }
+
+    public static Builder builder() {
+        return new Builder();
     }
 
     public String getName() {
@@ -31,6 +37,40 @@ public class Product {
 
     public int getPrice() {
         return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public static class Builder {
+
+        private String name;
+        private int price;
+        private int quantity;
+
+        public Builder name(String name) {
+            this.name = name;
+
+            return this;
+        }
+
+        public Builder price(int price) {
+            this.price = price;
+
+            return this;
+        }
+
+        public Builder quantity(int quantity) {
+            this.quantity = quantity;
+
+            return this;
+        }
+
+        public Product build() {
+            return new Product(this);
+        }
+
     }
 
 }
--- src/test/java/store/domain/product/ProductTest.java
+++ src/test/java/store/domain/product/ProductTest.java
@@ -9,7 +9,11 @@ public class ProductTest {
     @Test
     @DisplayName("상품을 생성한다")
     void 상품_생성_테스트() {
-        Product product = Product.of("콜라", 1000);
+        Product product = Product.builder()
+                .name("콜라")
+                .price(1000)
+                .quantity(10)
+                .build();
 
         Assertions.assertThat(product.getName()).isEqualTo("콜라");
         Assertions.assertThat(product.getPrice()).isEqualTo(1000);
@@ -18,7 +22,11 @@ public class ProductTest {
     @Test
     @DisplayName("상품 이름이 null이면 예외가 발생한다")
     void 상품_이름_null_예외가_발생한다() {
-        Assertions.assertThatThrownBy(() -> Product.of(null, 1000))
+        Assertions.assertThatThrownBy(() -> Product.builder()
+                        .name(null)
+                        .price(1000)
+                        .quantity(10)
+                        .build())
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR]");
     }
@@ -26,7 +34,11 @@ public class ProductTest {
     @Test
     @DisplayName("상품 이름이 빈 문자열이면 예외가 발생한다")
     void 상품_이름_빈_문자열_예외가_발생한다() {
-        Assertions.assertThatThrownBy(() -> Product.of("", 1000))
+        Assertions.assertThatThrownBy(() -> Product.builder()
+                        .name("")
+                        .price(1000)
+                        .quantity(10)
+                        .build())
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR]");
     }
@@ -34,7 +46,11 @@ public class ProductTest {
     @Test
     @DisplayName("상품 가격이 음수면 예외가 발생한다")
     void 상품_가격_음수면_예외가_발생한다() {
-        Assertions.assertThatThrownBy(() -> Product.of("콜라", -1000))
+        Assertions.assertThatThrownBy(() -> Product.builder()
+                        .name("cola")
+                        .price(-1000)
+                        .quantity(10)
+                        .build())
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR]");
     }
