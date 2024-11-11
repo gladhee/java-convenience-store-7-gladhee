f3f500112f6e5f9209a509c880754d2aa2202e3d
gladhee <gladhee@icloud.com>
2024-11-11 10:36:48 +0900

    feat(Application): 진입점 작성

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -1,7 +1,18 @@
 package store;
 
+import store.config.DatabaseConfig;
+import store.controller.StoreController;
+import store.service.OrderService;
+import store.service.StoreProductsService;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        DatabaseConfig databaseConfig = DatabaseConfig.getInstance();
+        OrderService orderService = new OrderService(databaseConfig.getStoreProducts());
+        StoreProductsService storeProductsService = new StoreProductsService(databaseConfig.getStoreProducts());
+        StoreController storeController = new StoreController(orderService, storeProductsService);
+
+        System.out.println(databaseConfig.getStoreProducts().findAll());
+        storeController.run();
     }
 }
