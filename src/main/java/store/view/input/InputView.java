a6c784098ba8ca91e5baa10bf7de9b1ad8c282de
gladhee <gladhee@icloud.com>
2024-11-11 13:51:34 +0900

    feat(InputView): 입력 View 작성

--- src/main/java/store/view/input/InputView.java
+++ src/main/java/store/view/input/InputView.java
@@ -4,29 +4,32 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-    public String inputProducts() {
+    public static String inputProducts() {
         System.out.println("구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])");
 
         return Console.readLine();
     }
 
-    public boolean askToMembershipUse() {
+    public static boolean askToMembershipUse() {
         System.out.println("멤버십 할인을 받으시겠습니까? (Y/N)");
 
         return inputYesOrNo();
     }
 
-    public boolean askToPromotionPurchase(String productName) {
+    public static boolean askToPromotionPurchase(String productName, int quantity) {
         StringBuilder stringBuilder = new StringBuilder();
 
-        stringBuilder.append("현재 ").append(productName).append("은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)");
+        stringBuilder.append("현재 ").append(productName)
+                .append("은(는) ")
+                .append(quantity)
+                .append("개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)");
 
         System.out.println(stringBuilder.toString());
 
         return inputYesOrNo();
     }
 
-    public boolean askToPromotionNotApplicablePurchase(String productName, int quantity) {
+    public static boolean askToPromotionNotApplicablePurchase(String productName, int quantity) {
         StringBuilder stringBuilder = new StringBuilder();
 
         stringBuilder.append("현재 ").append(productName).append(" ")
@@ -37,13 +40,13 @@ public class InputView {
         return inputYesOrNo();
     }
 
-    public boolean askToContinuePurchase() {
+    public static boolean askToContinuePurchase() {
         System.out.println("감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)");
 
         return inputYesOrNo();
     }
 
-    private boolean inputYesOrNo() {
+    private static boolean inputYesOrNo() {
         try {
             String input = Console.readLine().toUpperCase();
             validateYesOrNo(input);
@@ -55,7 +58,7 @@ public class InputView {
         }
     }
 
-    private void validateYesOrNo(String input) {
+    private static void validateYesOrNo(String input) {
         if (!input.equals("Y") && !input.equals("N")) {
             throw new IllegalArgumentException("[ERROR] Y 또는 N을 입력해 주세요.");
         }
